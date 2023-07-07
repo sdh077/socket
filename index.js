fe1bf501730583e3f0f1c3a7bba57aea2de1e891
shindaeho <sdh077@naver.com>
2023-07-07 14:34:07 +0900

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+
+app.use(cors());
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
